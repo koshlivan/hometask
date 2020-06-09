f2735a413af037eb4bd87a780989fe301ee27fb4
Ivan <66247725+koshlivan@users.noreply.github.com>
2020-06-09 06:47:44 +0300

    Add files via upload

---
+++ t1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    try {
+        int x;
+        cin >> x;
+        int y;
+        cin >> y;
+       if(y==0) throw 0;
+    }
+    catch (int y)
+    {
+        cout << "error\n";
+    }
+}
